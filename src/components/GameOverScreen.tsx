071bf2ed07f2ce3d02ef422e14805188dca3ee14
Akshay Mohan <akshay@sleeksky.com>
2025-05-08 02:28:13 +0530

    Flow changes

--- src/components/GameOverScreen.tsx
+++ src/components/GameOverScreen.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
-import { RefreshCw } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { RefreshCw, Trophy } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 interface GameOverScreenProps {
   score: number;
   onRestart: () => void;
-  onSubmitEmail: (email: string, name: string) => void;
+  onSubmitEmail: (
+    email: string,
+    name: string,
+    scoreId?: string | null
+  ) => Promise<string | null>;
 }
 
 function getLatestUser(type: string) {
@@ -15,9 +19,9 @@ function getLatestUser(type: string) {
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
 
-    if (key && key.startsWith('user-')) {
-      const user = JSON.parse(localStorage.getItem(key) || '{}');
-      
+    if (key?.startsWith("user-")) {
+      const user = JSON.parse(localStorage.getItem(key) || "{}");
+
       if (user.savedAt && user.savedAt > latestTimestamp) {
         latestTimestamp = user.savedAt;
         latestUser = { key, ...user };
@@ -26,44 +30,69 @@ function getLatestUser(type: string) {
   }
 
   if (latestUser) {
-    if(type === 'email')
-      return latestUser.email;
-    else if(type === 'name')
-      return latestUser.name;
+    if (type === "email") return latestUser.email;
+    else if (type === "name") return latestUser.name;
   } else {
-    return '';
+    return "";
   }
 }
 
-const GameOverScreen: React.FC<GameOverScreenProps> = ({ score, onRestart, onSubmitEmail }) => {
-  const [email, setEmail] = useState(getLatestUser('email'));
-  const [name, setName] = useState(getLatestUser('name'));
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState('');
+const GameOverScreen: React.FC<GameOverScreenProps> = ({
+  score,
+  onRestart,
+  onSubmitEmail,
+}) => {
+  const [email, setEmail] = useState(getLatestUser("email"));
+  const [name, setName] = useState(getLatestUser("name"));
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(
+    localStorage.getItem("submitted") !== "false"
+  );
+  const [error, setError] = useState("");
+  const [scoreId, setScoreId] = useState<string | null>(
+    localStorage.getItem("submitted") === "false"
+      ? null
+      : localStorage.getItem("submitted")
+  );
   const navigate = useNavigate();
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    
+  useEffect(() => {
+    if (email && name && !submitted && !loading) {
+      handleSubmit();
+    }
+  }, [submitted, loading]);
+
+  const handleUpdateName = () => {
+    setName("");
+    setEmail("");
+    setSubmitted(false);
+  };
+
+  const handleSubmit = async (e?: React.FormEvent) => {
+    e?.preventDefault();
+
+    setLoading(true);
     if (!name.trim()) {
-      setError('Please enter your name');
+      setError("Please enter your name");
       return;
     }
-    
+
     if (!email.trim()) {
-      setError('Please enter your email');
+      setError("Please enter your email");
       return;
     }
-    
+
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      setError('Please enter a valid email');
+      setError("Please enter a valid email");
       return;
     }
-    
-    onSubmitEmail(email,name);
+
+    const generatedScore = await onSubmitEmail(email, name, scoreId);
+    setScoreId(generatedScore);
     setSubmitted(true);
-    navigate('/leaderboard')
-    setError('');
+    localStorage.setItem("submitted", generatedScore ?? "true");
+    setError("");
+    setLoading(false);
   };
 
   return (
@@ -74,58 +103,97 @@ const GameOverScreen: React.FC<GameOverScreenProps> = ({ score, onRestart, onSub
           <p className="text-xl mb-1">Your Score</p>
           <p className="text-5xl font-bold text-yellow-300 mb-4">{score}</p>
         </div>
-        
-        {!submitted ? (
-          <>
-            <p className="text-center mb-4">Submit your email to join the leaderboard!</p>
-            
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div>
-                <input
-                  type="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="What do we call you?"
-                  className="w-full px-4 py-3 rounded-lg bg-white backdrop-blur-sm border border-white/20 text-black placeholder-black/50 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                />
-              </div>
-              <div>
-                <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Your email"
-                  className="w-full px-4 py-3 rounded-lg bg-white backdrop-blur-sm border border-white/20 text-black placeholder-black/50 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                />
-                {error && <p className="mt-1 text-red-300 text-sm">{error}</p>}
-              </div>
-              
-              <button 
-                type="submit" 
-                className=" w-full relative inline-block text-lg group"
-              >
-                <span className=" w-full relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-white transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-purpleBrand"></span>
-                  <span className="absolute left-0 w-screen h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-                  <span className="relative">Submit Score</span>
-                </span>
-                <span
-                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                  data-rounded="rounded-lg"
-                ></span>
-              </button>
-            </form>
-          </>
-        ) : (
-          <div className="text-center">
-            <div className="mb-4 p-3 bg-green-500/20 border border-green-500/30 rounded-lg">
-              <p className="text-green-300">Score submited</p>
-            </div>
+
+        {loading ? (
+          <div className="flex justify-center items-center h-64">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-white"></div>
           </div>
+        ) : (
+          <>
+            {!submitted ? (
+              <>
+                <p className="text-center mb-4">
+                  Submit your email to join the leaderboard!
+                </p>
+
+                <form onSubmit={handleSubmit} className="space-y-4">
+                  <div>
+                    <input
+                      type="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      placeholder="What do we call you?"
+                      className="w-full px-4 py-3 rounded-lg bg-white backdrop-blur-sm border border-white/20 text-black placeholder-black/50 focus:outline-none focus:ring-2 focus:ring-gray-900"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Your email"
+                      className="w-full px-4 py-3 rounded-lg bg-white backdrop-blur-sm border border-white/20 text-black placeholder-black/50 focus:outline-none focus:ring-2 focus:ring-gray-900"
+                    />
+                    {error && (
+                      <p className="mt-1 text-red-300 text-sm">{error}</p>
+                    )}
+                  </div>
+
+                  <button
+                    type="submit"
+                    className=" w-full relative inline-block text-lg group"
+                  >
+                    <span className=" w-full relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-white transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                      <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-purpleBrand"></span>
+                      <span className="absolute left-0 w-screen h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                      <span className="relative">Submit Score</span>
+                    </span>
+                    <span
+                      className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                      data-rounded="rounded-lg"
+                    ></span>
+                  </button>
+                </form>
+              </>
+            ) : (
+              <>
+                <div className="text-center">
+                  <div className="p-3 bg-green-500/20 border border-green-500/30 rounded-lg">
+                    <p className="text-green-300">
+                      {`Nice try, ${name}! Want to go again?`}
+                    </p>
+                  </div>
+                  <button
+                    onClick={handleUpdateName}
+                    className="mb-4 text-xs text-green-300 underline cursor-pointer rounded"
+                  >
+                    {`Not ${name}? You can update the user`}
+                  </button>
+                </div>
+                <button
+                  onClick={() => navigate("/leaderboard")}
+                  className=" w-full relative inline-block text-lg group mt-2"
+                >
+                  <span className=" w-full relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-white transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-purpleBrand"></span>
+                    <span className="absolute left-0 w-screen h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                    <span className="relative flex justify-center items-center">
+                      <Trophy size={18} className="text-yellow-300 mr-2" />
+                      Leaderboard
+                    </span>
+                  </span>
+                  <span
+                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                    data-rounded="rounded-lg"
+                  ></span>
+                </button>
+              </>
+            )}
+          </>
         )}
-        
-        <button 
-          onClick={onRestart} 
+
+        <button
+          onClick={onRestart}
           className=" w-full relative inline-block text-lg group mt-2"
         >
           <span className=" w-full relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-white transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
--- src/utils/gameUtils.ts
+++ src/utils/gameUtils.ts
@@ -1,28 +1,26 @@
-import { Cat, Player, Scores } from '../types';
-import { v4 as uuidv4 } from 'uuid';
-import { supabase } from './supabase';
+import { Cat, Player, Scores } from "../types";
+import { v4 as uuidv4 } from "uuid";
+import { supabase } from "./supabase";
 
 // Cat images
 const CAT_IMAGES = {
   common: [
-    '/assets/stickers/image_768.png',
-    '/assets/stickers/image_769.png',
-    '/assets/stickers/image_771.png',
-    '/assets/stickers/image_773.png',
-    '/assets/stickers/image_774.png',
-    '/assets/stickers/image_770.png',
-    '/assets/stickers/image_775.png',
-    '/assets/stickers/image_772.png'
+    "/assets/stickers/image_768.png",
+    "/assets/stickers/image_769.png",
+    "/assets/stickers/image_771.png",
+    "/assets/stickers/image_773.png",
+    "/assets/stickers/image_774.png",
+    "/assets/stickers/image_770.png",
+    "/assets/stickers/image_775.png",
+    "/assets/stickers/image_772.png",
   ],
   rare: [
-    '/assets/stickers/Gold_1.svg',
-    '/assets/stickers/Gold_2.svg',
-    '/assets/stickers/Gold_3.svg',
-    '/assets/stickers/Gold_4.svg'
+    "/assets/stickers/Gold_1.svg",
+    "/assets/stickers/Gold_2.svg",
+    "/assets/stickers/Gold_3.svg",
+    "/assets/stickers/Gold_4.svg",
   ],
-  legendary: [
-    '/assets/stickers/Super_rare.svg'
-  ]
+  legendary: ["/assets/stickers/Super_rare.svg"],
 };
 
 export const getRandomPosition = (width: number, height: number) => {
@@ -32,130 +30,150 @@ export const getRandomPosition = (width: number, height: number) => {
   return { x, y };
 };
 
-
-
 export const generateCat = (width: number, height: number): Cat => {
   const rand = Math.random();
-  let type: 'common' | 'rare' | 'legendary';
+  let type: "common" | "rare" | "legendary";
   let points: number;
   let speed: number;
-  
+
   if (rand < 0.7) {
-    type = 'common';
+    type = "common";
     points = 1;
     speed = 0.3 + Math.random() * 0.4;
   } else if (rand < 0.95) {
-    type = 'rare';
+    type = "rare";
     points = 3;
     speed = 0.7 + Math.random() * 0.5;
   } else {
-    type = 'legendary';
+    type = "legendary";
     points = 5;
     speed = 1 + Math.random() * 0.7;
   }
-  
+
   const images = CAT_IMAGES[type];
   const image = images[Math.floor(Math.random() * images.length)];
-  
+
   return {
     id: uuidv4(),
     image,
     points,
     type,
     speed,
-    position: getRandomPosition(width, height)
+    position: getRandomPosition(width, height),
   };
 };
 
 export const getLeaderboardData = async (): Promise<Scores[]> => {
   const { data: scores, error } = await supabase
-    .from('scores')
-    .select(`
+    .from("scores")
+    .select(
+      `
       score,
       profiles (
         *
       ),
       created_at
-    `)
-    .order('score', { ascending: false })
+    `
+    )
+    .order("score", { ascending: false })
     .limit(10);
 
-
-
   if (error) {
-    console.error('Error fetching leaderboard:', error);
+    console.error("Error fetching leaderboard:", error);
     return [];
   }
 
-  return scores.map(score => ({
+  return scores.map((score) => ({
     user_id: score.profiles.id,
     name: score.profiles.name,
     score: score.score,
-    date: new Date(score.created_at).toLocaleDateString()
+    date: new Date(score.created_at).toLocaleDateString(),
   }));
 };
 
-export const sendVerificationEmail = async (email: string, name: string, score: number): Promise<boolean> => {
+export const sendVerificationEmail = async (
+  email: string,
+  name: string,
+  score: number,
+  scoreId?: string | null
+): Promise<null | string> => {
   try {
-
     let currentUser = null;
 
-    try{
+    try {
       const { data: userProfile, error: userProfileError } = await supabase
-        .from('profiles')
-        .select('id, name')
-        .eq('email', email) // ⬅️ filter by the field you want
-        .single()
+        .from("profiles")
+        .select("id, name")
+        .eq("email", email) // ⬅️ filter by the field you want
+        .single();
 
-      if(!userProfileError && userProfile){
+      if (!userProfileError && userProfile) {
         currentUser = userProfile;
-      } 
-    } catch(e) {
-      console.error('user not in profiles');
+      }
+    } catch (e) {
+      console.error("user not in profiles", e);
     }
 
-    if(!currentUser){
+    if (!currentUser) {
       // Try to sign up first
-      const { data: { user }, error: signUpError } = await supabase.auth.signUp({
+      const {
+        data: { user },
+        error: signUpError,
+      } = await supabase.auth.signUp({
         email,
         password: uuidv4(),
         options: {
           data: {
-            name
-          }
-        }
+            name,
+          },
+        },
       });
 
-      if(!signUpError && user){
+      if (!signUpError && user) {
         const { data: userProfile } = await supabase
-          .from('profiles')
-          .select('id, name')
-          .eq('email', email) // ⬅️ filter by the field you want
-          .single()
+          .from("profiles")
+          .select("id, name")
+          .eq("email", email) // ⬅️ filter by the field you want
+          .single();
         currentUser = userProfile;
       }
     }
-    
-    if (!currentUser) throw new Error('No user returned from signup');
+
+    if (!currentUser) throw new Error("No user returned from signup");
     // Store new user into localStorage
-    localStorage.setItem(`user-${email}`, JSON.stringify({...currentUser, savedAt: Date.now(), email}));
+    localStorage.setItem(
+      `user-${email}`,
+      JSON.stringify({ ...currentUser, savedAt: Date.now(), email })
+    );
 
-    // Insert the score
-    const { error: scoreError } = await supabase
-      .from('scores')
-      .insert([
-        {
-          user_id: currentUser.id,
-          score: score
-        }
-      ]);
+    let generatedId;
 
-    if (scoreError) throw scoreError;
+    if (scoreId) {
+      const { data: updatedData, error: updateError } = await supabase
+        .from("scores")
+        .update({ user_id: currentUser.id })
+        .eq("id", scoreId) // scoreId is the ID of the row you want to update
+        .select(); // optional: returns the updated row
+      if (updateError) throw updateError;
+      generatedId = updatedData[0].id;
+    } else {
+      // Insert the score
+      const { data: insertedData, error: scoreError } = await supabase
+        .from("scores")
+        .insert([
+          {
+            user_id: currentUser.id,
+            score: score,
+          },
+        ])
+        .select();
+      if (scoreError) throw scoreError;
+      generatedId = insertedData[0].id;
+    }
 
-    return true;
+    return generatedId;
   } catch (error) {
-    console.error('Error during signup/login/score insert:', error);
-    return false;
+    console.error("Error during signup/login/score insert:", error);
+    return null;
   }
 };
-
